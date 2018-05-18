a9d74f737e72aa41128ff2794cbdede57ced4105
poljar (Damir Jelić) <poljar@termina.org.uk>
2018-05-18 14:07:07 +0200

    tests: Add response parsing tests.

---
+++ tests/responses_test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import json
+
+from nio.responses import ErrorResponse, LoginResponse
+
+
+class TestClass(object):
+    @staticmethod
+    def _load_response(filename):
+        # type: (str) -> Dict[Any, Any]
+        with open(filename) as f:
+            return json.loads(f.read(), encoding="utf-8")
+
+    def test_login_parse(self):
+        parsed_dict = TestClass._load_response(
+            "tests/data/login_response.json")
+        response = LoginResponse.from_dict(parsed_dict)
+        assert isinstance(response, LoginResponse)
+
+    def test_login_failure_parse(self):
+        parsed_dict = TestClass._load_response(
+            "tests/data/login_response_error.json")
+        response = LoginResponse.from_dict(parsed_dict)
+        assert isinstance(response, ErrorResponse)
