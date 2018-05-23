db6914d3a59e9ba420c5d83d6c6666a10b4f6cd9
poljar (Damir Jelić) <poljar@termina.org.uk>
2018-05-23 19:31:19 +0200

    tests: Add initial http tests.

---
+++ tests/http_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from nio.client import HttpClient, RemoteTransportError
+
+
+class TestClass(object):
+    @staticmethod
+    def _load_response(filename):
+        # type: (str) -> Dict[Any, Any]
+        with open(filename, "rb") as f:
+            return f.read()
+
+    def test_503(self):
+        client = HttpClient("localhost", "example")
+        client.connect()
+        client.login("test")
+        transport_response = self._load_response("tests/data/http_503.txt")
+        try:
+            client.receive(transport_response)
+        except RemoteTransportError:
+            pass
+
+    def test_502(self):
+        client = HttpClient("localhost", "example")
+        client.connect()
+        client.login("test")
+        transport_response = self._load_response("tests/data/http_502.txt")
+        try:
+            client.receive(transport_response)
+        except RemoteTransportError:
+            pass
