952ad77695f9af3995308789053078cec3e2d7bb
Damir Jelić <poljar@termina.org.uk>
2019-01-18 18:56:55 +0100

    tests: Add more client tests.

--- tests/client_test.py
+++ tests/client_test.py
@@ -15,7 +15,11 @@ from nio import (
     RoomInfo,
     Timeline,
     RoomMemberEvent,
-    RoomEncryptionEvent
+    RoomEncryptionEvent,
+    RoomSummary,
+    KeysQueryResponse,
+    JoinedMembersResponse,
+    RoomMember
 )
 
 HOST = "example.org"
@@ -24,6 +28,9 @@ DEVICE_ID = "DEVICEID"
 
 BOB_ID = "@bob:example.org"
 TEST_ROOM_ID = "!testroom:example.org"
+
+ALICE_ID = "@alice:example.org"
+ALICE_DEVICE_ID = "JLAFKJWSCS"
 
 
 class TestClass(object):
@@ -37,17 +44,17 @@ class TestClass(object):
             [
                 RoomMemberEvent(
                     "event_id_1",
-                    BOB_ID,
+                    ALICE_ID,
                     1516809890615,
-                    BOB_ID,
+                    ALICE_ID,
                     {"membership": "join"}
                 ),
-                RoomEncryptionEvent("event_id_2", BOB_ID, 1516809890615)
+                RoomEncryptionEvent("event_id_2", ALICE_ID, 1516809890615)
             ],
             False,
             "prev_batch_token"
         )
-        test_room_info = RoomInfo(timeline, [], [], [], None)
+        test_room_info = RoomInfo(timeline, [], [], [], RoomSummary(1, 2, []))
         rooms = Rooms(
             {},
             {
@@ -59,8 +66,42 @@ class TestClass(object):
             "token123",
             rooms,
             DeviceOneTimeKeyCount(49, 50),
-            DeviceList([BOB_ID], []),
+            DeviceList([ALICE_ID], []),
             []
+        )
+
+    @property
+    def keys_query_response(self):
+        return KeysQueryResponse(
+            {
+                "@alice:example.org": {
+                    "JLAFKJWSCS": {
+                        "algorithms": ["m.olm.v1.curve25519-aes-sha2", "m.megolm.v1.aes-sha2"],
+                        "device_id": "JLAFKJWSCS",
+                        "user_id": "@alice:example.org",
+                        "keys": {
+                            "curve25519:JLAFKJWSCS": "wjLpTLRqbqBzLs63aYaEv2Boi6cFEbbM/sSRQ2oAKk4",
+                            "ed25519:JLAFKJWSCS": "nE6W2fCblxDcOFmeEtCHNl8/l8bXcu7GKyAswA4r3mM"
+                        },
+                        "signatures": {
+                            "@alice:example.org": {
+                                "ed25519:JLAFKJWSCS": "m53Wkbh2HXkc3vFApZvCrfXcX3AI51GsDHustMhKwlv3TuOJMj4wistcOTM8q2+e/Ro7rWFUb9ZfnNbwptSUBA"
+                            }
+                        }
+                    }
+                }
+            },
+            {},
+        )
+
+    @property
+    def joined_members(self):
+        return JoinedMembersResponse(
+            [
+                RoomMember(BOB_ID, None, None),
+                RoomMember(ALICE_ID, None, None),
+            ],
+            TEST_ROOM_ID
         )
 
     def test_client_protocol_error(self):
@@ -94,6 +135,12 @@ class TestClass(object):
 
         with pytest.raises(LocalProtocolError):
             client.share_group_session(None)
+
+        with pytest.raises(LocalProtocolError):
+            client.keys_claim(None)
+
+        with pytest.raises(LocalProtocolError):
+            client.keys_query(None)
 
     def test_client_create(self, client):
         assert isinstance(client, Client)
@@ -141,3 +188,38 @@ class TestClass(object):
 
         assert room.encrypted
         assert client.should_query_keys
+
+    def test_client_key_query(self, client):
+        client.receive_response(self.login_response)
+        client.receive_response(KeysUploadResponse(50, 50))
+
+        assert not client.should_query_keys
+        client.receive_response(self.sync_response)
+
+        assert client.rooms[TEST_ROOM_ID]
+        room = client.rooms[TEST_ROOM_ID]
+
+        assert room.encrypted
+        assert room.summary
+        assert len(room.users) == 1
+        assert room.member_count == 2
+        assert room.summary.joined_member_count == 2
+        assert client.should_query_keys
+        assert not client.device_store.users
+
+        client.receive_response(self.keys_query_response)
+
+        assert not client.should_query_keys
+        assert client.device_store.users
+
+        alice_device = client.device_store[ALICE_ID][ALICE_DEVICE_ID]
+        assert alice_device
+
+        assert not room.members_synced
+
+        client.receive_response(self.joined_members)
+
+        assert room.members_synced
+        assert client.should_query_keys
+
+        assert client.users_for_key_query == set([BOB_ID])
