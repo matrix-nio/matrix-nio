f5e9e8df9a245bd3e84948ef327f2a200bc0190a
Damir Jelić <poljar@termina.org.uk>
2019-01-21 15:41:40 +0100

    tests: Add a tests to make sure the key query logic is correct.

--- tests/client_test.py
+++ tests/client_test.py
@@ -71,6 +71,38 @@ class TestClass(object):
         )
 
     @property
+    def second_sync(self):
+        timeline = Timeline(
+            [
+                RoomMemberEvent(
+                    "event_id_1",
+                    ALICE_ID,
+                    1516809890615,
+                    ALICE_ID,
+                    {"membership": "join"}
+                ),
+                RoomEncryptionEvent("event_id_2", ALICE_ID, 1516809890615)
+            ],
+            True,
+            "prev_batch_token"
+        )
+        test_room_info = RoomInfo(timeline, [], [], [], RoomSummary(1, 2, []))
+        rooms = Rooms(
+            {},
+            {
+                TEST_ROOM_ID: test_room_info
+            },
+            {}
+        )
+        return SyncResponse(
+            "token123",
+            rooms,
+            DeviceOneTimeKeyCount(49, 50),
+            DeviceList([], []),
+            []
+        )
+
+    @property
     def keys_query_response(self):
         return KeysQueryResponse(
             {
@@ -190,6 +222,8 @@ class TestClass(object):
         assert client.should_query_keys
 
     def test_client_key_query(self, client):
+        assert not client.should_query_keys
+
         client.receive_response(self.login_response)
         client.receive_response(KeysUploadResponse(50, 50))
 
@@ -223,3 +257,39 @@ class TestClass(object):
         assert client.should_query_keys
 
         assert client.users_for_key_query == set([BOB_ID])
+
+    def test_query_rule(self, tempdir):
+        client = Client("ephemeral", "DEVICEID", tempdir)
+        client.receive_response(self.login_response)
+        client.receive_response(KeysUploadResponse(50, 50))
+        assert not client.should_query_keys
+
+        client.receive_response(self.sync_response)
+        assert client.should_query_keys
+        client.receive_response(self.keys_query_response)
+        assert client.olm.tracked_users == set([ALICE_ID])
+        assert not client.should_query_keys
+
+        del client
+
+        client = Client("ephemeral", "DEVICEID", tempdir)
+        client.receive_response(self.login_response)
+        assert not client.should_upload_keys
+        assert not client.should_query_keys
+
+        alice_device = client.device_store[ALICE_ID][ALICE_DEVICE_ID]
+        assert alice_device
+
+        client.receive_response(self.second_sync)
+        assert client.should_query_keys
+
+        client.users_for_key_query == set([ALICE_ID])
+
+        client.receive_response(self.joined_members)
+
+        client.users_for_key_query == set([ALICE_ID, BOB_ID])
+
+        client.receive_response(self.keys_query_response)
+        assert client.olm.tracked_users == set([ALICE_ID])
+        assert client.users_for_key_query == set([BOB_ID])
+        assert client.should_query_keys
