4bf1e0e1b9bab7dee6a484aaa695a3e5b3570729
Damir Jelić <poljar@termina.org.uk>
2019-03-22 16:14:31 +0100

    tests: Add a keys query test for the async client.

--- tests/async_client_test.py
+++ tests/async_client_test.py
@@ -7,8 +7,22 @@ from nio import (
     SyncResponse,
     LoginError,
     LocalProtocolError,
-    KeysUploadResponse
+    KeysUploadResponse,
+    Rooms,
+    RoomInfo,
+    Timeline,
+    RoomMemberEvent,
+    RoomEncryptionEvent,
+    RoomSummary,
+    DeviceOneTimeKeyCount,
+    DeviceList
 )
+
+TEST_ROOM_ID = "!testroom:example.org"
+
+ALICE_ID = "@alice:example.org"
+ALICE_DEVICE_ID = "JLAFKJWSCS"
+
 
 if sys.version_info >= (3, 5):
     import asyncio
@@ -32,6 +46,43 @@ class TestClass(object):
     @property
     def sync_response(self):
         return self._load_response("tests/data/sync.json")
+
+    @property
+    def keys_query_response(self):
+        return self._load_response(
+            "tests/data/keys_query.json")
+
+    @property
+    def encryption_sync_response(self):
+        timeline = Timeline(
+            [
+                RoomMemberEvent(
+                    "event_id_1",
+                    ALICE_ID,
+                    1516809890615,
+                    ALICE_ID,
+                    {"membership": "join"}
+                ),
+                RoomEncryptionEvent("event_id_2", ALICE_ID, 1516809890615)
+            ],
+            False,
+            "prev_batch_token"
+        )
+        test_room_info = RoomInfo(timeline, [], [], [], RoomSummary(1, 2, []))
+        rooms = Rooms(
+            {},
+            {
+                TEST_ROOM_ID: test_room_info
+            },
+            {}
+        )
+        return SyncResponse(
+            "token123",
+            rooms,
+            DeviceOneTimeKeyCount(49, 50),
+            DeviceList([ALICE_ID], []),
+            []
+        )
 
     def test_login(self, async_client, aioresponse):
         loop = asyncio.get_event_loop()
@@ -120,3 +171,25 @@ class TestClass(object):
         assert isinstance(resp2, KeysUploadResponse)
         assert async_client.olm_account_shared
         assert async_client.should_upload_keys
+
+    def test_keys_query(self, async_client, aioresponse):
+        loop = asyncio.get_event_loop()
+        aioresponse.post(
+            "https://example.org/_matrix/client/r0/login",
+            status=200,
+            payload=self.login_response
+        )
+        aioresponse.post(
+            "https://example.org/_matrix/client/r0/keys/query?access_token=abc123",
+            status=200,
+            payload=self.keys_query_response
+        )
+
+        loop.run_until_complete(async_client.login("wordpass"))
+        assert not async_client.should_query_keys
+
+        async_client.receive_response(self.encryption_sync_response)
+        assert async_client.should_query_keys
+
+        loop.run_until_complete(async_client.keys_query())
+        assert not async_client.should_query_keys
