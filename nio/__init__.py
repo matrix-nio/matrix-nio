b17afc8e6b74062e7310b8af3206016f03416e7d
Damir Jelić <poljar@termina.org.uk>
2019-03-22 10:09:01 +0100

    nio: Use a wildcard import for the client now.

--- nio/__init__.py
+++ nio/__init__.py
@@ -1,5 +1,5 @@
 from .log import logger_group
-from .client import HttpClient, TransportType, Client, AsyncClient
+from .client import *
 from .api import MessageDirection, Api
 from .responses import *
 from .events import *
