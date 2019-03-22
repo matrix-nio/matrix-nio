28edbd78a78d5db3217f01cf5602ba4c460922dd
Damir Jelić <poljar@termina.org.uk>
2019-03-22 13:38:10 +0100

    tests: More async client tests.

--- tests/async_client_test.py
+++ tests/async_client_test.py
@@ -2,7 +2,7 @@ import sys
 import json
 import pytest
 
-from nio import LoginResponse, SyncResponse
+from nio import LoginResponse, SyncResponse, LoginError, LocalProtocolError
 
 if sys.version_info >= (3, 5):
     import asyncio
@@ -40,6 +40,26 @@ class TestClass(object):
         assert async_client.access_token
         assert async_client.logged_in
 
+    def test_failed_login(self, async_client, aioresponse):
+        loop = asyncio.get_event_loop()
+
+        assert not async_client.access_token
+        assert not async_client.logged_in
+
+        aioresponse.post(
+            "https://example.org/_matrix/client/r0/login",
+            status=400,
+            body=""
+        )
+        resp = loop.run_until_complete(async_client.login("wordpass"))
+        assert isinstance(resp, LoginError)
+        assert not async_client.logged_in
+
+        assert async_client.client_session
+        async_client.close()
+        loop.run_until_complete(async_client.close())
+        assert not async_client.client_session
+
     def test_sync(self, async_client, aioresponse):
         loop = asyncio.get_event_loop()
 
@@ -53,6 +73,8 @@ class TestClass(object):
             status=200,
             payload=self.sync_response
         )
+        with pytest.raises(LocalProtocolError):
+            resp2 = loop.run_until_complete(async_client.sync())
 
         resp = loop.run_until_complete(async_client.login("wordpass"))
         resp2 = loop.run_until_complete(async_client.sync())
