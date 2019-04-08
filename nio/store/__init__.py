67809df261118303d346807afcafa735992dc132
Damir Jelić <poljar@termina.org.uk>
2019-04-08 16:36:07 +0200

    store: Expose some more functions to users.
    This way library users can modify and extend the store to their liking.

--- nio/store/__init__.py
+++ nio/store/__init__.py
@@ -1,4 +1,10 @@
 from .log import logger
 from .models import *
 from .file_trustdb import Key, KeyStore, Ed25519Key
-from .database import DefaultStore, MatrixStore, LegacyMatrixStore
+from .database import (
+    DefaultStore,
+    MatrixStore,
+    LegacyMatrixStore,
+    use_database,
+    use_database_atomic
+)
