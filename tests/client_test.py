9e8f412561c1e453c174462c00c671b002c86395
miruka <miruka@disroot.org>
2019-07-02 08:27:03 -0400

    Fix test_homeserver_url_parsing method name

--- tests/client_test.py
+++ tests/client_test.py
@@ -786,7 +786,7 @@ class TestClass(object):
         with pytest.raises(CallbackException):
             client.receive_response(self.sync_invite_response)
 
-    def test_homeserver_ulr_parsing(self):
+    def test_homeserver_url_parsing(self):
         host, path = HttpClient._parse_homeserver("https://example.org:8080")
         assert host == "example.org:8080"
         assert path == ""
