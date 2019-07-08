9f258ab8c9bbc4cc70200f7635681ae954b3bce0
Damir Jelić <poljar@termina.org.uk>
2019-07-08 16:19:21 +0200

    crypto: Add some crypto module documentation.

--- nio/crypto/__init__.py
+++ nio/crypto/__init__.py
@@ -1,3 +1,15 @@
+"""nio encryption module.
+
+Encryption is handled mostly transparently to the user.
+
+The main thing users need to worry about is device verification.
+
+While device verification is handled in the Client classes of nio the classes
+that are used to introspect OlmDevices or device authentication sessions are
+documented here.
+
+"""
+
 from .._compat import package_installed
 from .attachments import encrypt_attachment, decrypt_attachment
 
