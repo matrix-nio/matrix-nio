123512faf518e9131c444420237f3f528e67f09e
Damir Jelić <poljar@termina.org.uk>
2019-07-11 16:31:38 +0200

    doc: Include the README in the docs.

--- doc/conf.py
+++ doc/conf.py
@@ -49,6 +49,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
+    'm2r',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,8 +58,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
--- doc/index.rst
+++ doc/index.rst
@@ -3,16 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to nio's documentation!
-===============================
+.. mdinclude:: ../README.md
 
-Contents
---------
+Api Documentation
+=================
 
 .. toctree::
    nio
    :maxdepth: 2
-
 
 
 Indices and tables
