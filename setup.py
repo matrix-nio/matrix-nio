a7a35f18f9bc2f031647c48870aea0f80ff92960
Damir Jelić <poljar@termina.org.uk>
2019-07-12 10:27:42 +0200

    nio: Bump the version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="matrix-nio",
-    version="0.3",
+    version="0.4",
     url="https://github.com/poljar/matrix-nio",
     author='Damir Jelić',
     author_email="poljar@termina.org.uk",
