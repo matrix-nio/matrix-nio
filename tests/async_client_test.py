cce529b4d6f8dc5e13ac86d834354aee8c22e449
Thomas Renard <Thomas.Renard@g3la.de>
2019-08-04 14:09:07 +0200

    test failed logout

--- tests/async_client_test.py
+++ tests/async_client_test.py
@@ -343,6 +343,31 @@ class TestClass(object):
         assert not async_client.access_token
         assert not async_client.logged_in
 
+    def test_failed_logout(self, async_client, aioresponse):
+        loop = asyncio.get_event_loop()
+
+        aioresponse.post(
+            "https://example.org/_matrix/client/r0/login",
+            status=200,
+            payload=self.login_response
+        )
+
+        aioresponse.post(
+            "https://example.org/_matrix/client/r0/logout?access_token=abc123",
+            status=400,
+            body=""
+        )
+
+        resp = loop.run_until_complete(async_client.login("wordpass"))
+        assert async_client.access_token
+        assert async_client.logged_in
+        resp2 = loop.run_until_complete(async_client.logout())
+
+        assert isinstance(resp, LoginResponse)
+        assert isinstance(resp2, LogoutError)
+        assert async_client.access_token
+        assert async_client.logged_in
+
     def test_sync(self, async_client, aioresponse):
         loop = asyncio.get_event_loop()
 
