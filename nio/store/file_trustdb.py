7cd187ad8cd7e0f29f036d60cdcf046187e3979d
Damir Jelić <poljar@termina.org.uk>
2020-01-29 15:46:07 +0100

    file_trustdb: Format and type hint conversion.

--- nio/store/file_trustdb.py
+++ nio/store/file_trustdb.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from functools import wraps
-from typing import DefaultDict, Dict, Iterator, List, Optional
+from typing import Any, Iterator, List, Optional
 
 from atomicwrites import atomic_write
 
@@ -14,15 +16,13 @@ except NameError:  # pragma: no cover
 
 
 class Key(object):
-    def __init__(self, user_id, device_id, key):
-        # type: (str, str, str) -> None
+    def __init__(self, user_id: str, device_id: str, key: str):
         self.user_id = user_id
         self.device_id = device_id
         self.key = key
 
     @classmethod
-    def from_line(cls, line):
-        # type: (str) -> Optional[Key]
+    def from_line(cls, line: str) -> Optional[Key]:
         fields = line.split(" ")
 
         if len(fields) < 4:
@@ -35,8 +35,7 @@ class Key(object):
         else:
             return None
 
-    def to_line(self):
-        # type: () -> str
+    def to_line(self) -> str:
         key_type = ""
 
         if isinstance(self, Ed25519Key):
@@ -52,16 +51,14 @@ class Key(object):
         return line
 
     @classmethod
-    def from_olmdevice(cls, device):
-        # type: (OlmDevice) -> Ed25519Key
+    def from_olmdevice(cls, device: OlmDevice) -> Ed25519Key:
         user_id = device.user_id
         device_id = device.id
         return Ed25519Key(user_id, device_id, device.ed25519)
 
 
 class Ed25519Key(Key):
-    def __eq__(self, value):
-        # type: (object) -> bool
+    def __eq__(self, value: Any) -> bool:
         if not isinstance(value, Ed25519Key):
             return NotImplemented
 
@@ -76,24 +73,20 @@ class Ed25519Key(Key):
 
 
 class KeyStore(object):
-    def __init__(self, filename):
-        # type: (str) -> None
-        self._entries = []  # type: List[Key]
-        self._filename = filename  # type: str
+    def __init__(self, filename: str):
+        self._entries: List[Key] = []
+        self._filename: str = filename
 
         self._load(filename)
 
-    def __iter__(self):
-        # type: () -> Iterator[Key]
+    def __iter__(self) -> Iterator[Key]:
         for entry in self._entries:
             yield entry
 
-    def __repr__(self):
-        # type: () -> str
+    def __repr__(self) -> str:
         return "KeyStore object, file: {}".format(self._filename)
 
-    def _load(self, filename):
-        # type: (str) -> None
+    def _load(self, filename: str):
         try:
             with open(filename, "r") as f:
                 for line in f:
@@ -111,8 +104,7 @@ class KeyStore(object):
         except FileNotFoundError:
             pass
 
-    def get_key(self, user_id, device_id):
-        # type: (str, str) -> Optional[Key]
+    def get_key(self, user_id: str, device_id: str) -> Optional[Key]:
         for entry in self._entries:
             if user_id == entry.user_id and device_id == entry.device_id:
                 return entry
@@ -129,19 +121,17 @@ class KeyStore(object):
         return decorated
 
     def _save(self):
-        # type: () -> None
         with atomic_write(self._filename, overwrite=True) as f:
             for entry in self._entries:
                 line = entry.to_line()
                 f.write(line)
 
     @_save_store # type: ignore
-    def add_many(self, keys):
-        # type: (List[Key]) -> None
+    def add_many(self, keys: List[Key]):
         for key in keys:
             self._add_without_save(key)
 
-    def _add_without_save(self, key):
+    def _add_without_save(self, key: Key) -> bool:
         existing_key = self.get_key(key.user_id, key.device_id)
 
         if existing_key:
@@ -164,26 +154,22 @@ class KeyStore(object):
         return True
 
     @_save_store # type: ignore
-    def add(self, key):
-        # type: (Key) -> bool
+    def add(self, key: Key) -> bool:
         return self._add_without_save(key)
 
     @_save_store # type: ignore
-    def remove_many(self, keys):
-        # type: (List[Key]) -> None
+    def remove_many(self, keys: List[Key]):
         for key in keys:
             if key in self._entries:
                 self._entries.remove(key)
 
     @_save_store # type: ignore
-    def remove(self, key):
-        # type: (Key) -> bool
+    def remove(self, key: Key) -> bool:
         if key in self._entries:
             self._entries.remove(key)
             return True
 
         return False
 
-    def check(self, key):
-        # type: (Key) -> bool
+    def check(self, key: Key) -> bool:
         return key in self._entries
