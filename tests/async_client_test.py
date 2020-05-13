402946d739cb34f2064d8e1c00fed89a84e40267
devNan0 <nan0@nan0.dev>
2020-05-13 03:41:50 +0200

    Fix test for update_device. Take 2

--- tests/async_client_test.py
+++ tests/async_client_test.py
@@ -1816,8 +1816,11 @@ class TestClass:
         device_id = "QBUAZIFURK"
         content = {"display_name": "My new device"}
 
-        aioresponse.post(
-            "https://example.org/_matrix/client/r0/devices/{}?access_token=abc123".format(device_id),
+        aioresponse.put(
+            "https://example.org/_matrix/client/r0/devices/{}?access_token={}".format(
+                device_id,
+                async_client.access_token
+            ),
             status=200,
             payload={}
         )
