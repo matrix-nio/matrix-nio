f0785f0e89b59dd359387f3e6df2a5cceef95cd3
Damir Jelić <poljar@termina.org.uk>
2020-06-21 21:32:37 +0200

    nio: Bump the version.

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ author = 'Damir Jelić'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.13.0'
+release = '0.14.0'
 
 
 # -- General configuration ---------------------------------------------------
