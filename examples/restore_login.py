56ff315f597007aef808fedc1e9181d14e39f8f3
8go <cccc@tuta.io>
2020-07-09 17:21:50 +0000

    incorporated suggestions from reviewer
    thanks for the feedback

--- examples/restore_login.py
+++ examples/restore_login.py
@@ -13,21 +13,21 @@ CONFIG_FILE = "restore_login_config.json"
 # Check out main() below to see how it's done.
 
 
-def write_details_to_disk(resp: LoginResponse, home_server) -> None:
+def write_details_to_disk(resp: LoginResponse, homeserver) -> None:
     """Writes the required login details to disk so we can log in later without
     using a password.
-
+        
     Arguments:
         resp {LoginResponse} -- the successful client login response.
-        home_server -- the name of the homeserver, e.g. "https://matrix.example.org"
+        homeserver -- URL of homeserver, e.g. "https://matrix.example.org"
     """
     # open the config file in write-mode
     with open(CONFIG_FILE, "w") as f:
         # write the login details to disk
         json.dump(
             {
-                "home_server": home_server,  # e.g. "https://matrix.example.org"
-                "access_token": resp.access_token,  # long cryptographic access token
+                "homeserver": homeserver,  # e.g. "https://matrix.example.org"
+                "access_token": resp.access_token,  # cryptogr. access token
                 "device_id": resp.device_id,  # device ID, 10 uppercase letters
                 "user_id": resp.user_id  # e.g. "@user:example.org"
             },
@@ -39,28 +39,29 @@ async def main() -> None:
 
     # If there are no previously-saved credentials, we'll use the password
     if not os.path.exists(CONFIG_FILE):
-        print("First time use. Did not find credential file. Asking for homeserver, user, and password to create credential file.")
-        home_server = "https://matrix.example.org"
-        home_server = input(f"Enter your homeserver: [{home_server}] ")
-        if not (home_server.startswith("https://")
-                or home_server.startswith("https://")):
-            home_server = "https://" + home_server
+        print("First time use. Did not find credential file. Asking for "
+              "homeserver, user, and password to create credential file.")
+        homeserver = "https://matrix.example.org"
+        homeserver = input(f"Enter your homeserver URL: [{homeserver}] ")
+        if not (homeserver.startswith("https://")
+                or homeserver.startswith("http://")):
+            homeserver = "https://" + homeserver
         user_id = "@user:example.org"
         user_id = input(f"Enter your full user ID: [{user_id}] ")
-        client = AsyncClient(home_server, user_id)
+        client = AsyncClient(homeserver, user_id)
         pw = getpass.getpass()
         resp = await client.login(pw)
         # check that we logged in succesfully
         if (isinstance(resp, LoginResponse)):
-            write_details_to_disk(resp, home_server)
+            write_details_to_disk(resp, homeserver)
         else:
-            print(f"homeserver = \"{home_server}\"; user = \"{user_id}\"")
+            print(f"homeserver = \"{homeserver}\"; user = \"{user_id}\"")
             print(f"Failed to log in: {resp}")
             sys.exit(1)
 
         print(
             "Logged in using a password. Credentials were stored.",
-            "Try running the script again to login with credentials (an access token)."
+            "Try running the script again to login with credentials."
         )
 
     # Otherwise the config file exists, so we'll use the stored credentials
@@ -68,7 +69,7 @@ async def main() -> None:
         # open the file in read-only mode
         with open(CONFIG_FILE, "r") as f:
             config = json.load(f)
-            client = AsyncClient(config['home_server'])
+            client = AsyncClient(config['homeserver'])
             client.access_token = config['access_token']
             client.user_id = config['user_id']
             client.device_id = config['device_id']
@@ -90,3 +91,4 @@ async def main() -> None:
     await client.close()
 
 asyncio.get_event_loop().run_until_complete(main())
+
