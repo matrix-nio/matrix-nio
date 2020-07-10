a4d8400a046ce61ac083adac891adc767461e412
Damir Jelić <poljar@termina.org.uk>
2020-07-10 18:42:17 +0200

    examples: Add some whitespace to the restore login example.

--- examples/restore_login.py
+++ examples/restore_login.py
@@ -16,7 +16,7 @@ CONFIG_FILE = "restore_login_config.json"
 def write_details_to_disk(resp: LoginResponse, homeserver) -> None:
     """Writes the required login details to disk so we can log in later without
     using a password.
-        
+
     Arguments:
         resp {LoginResponse} -- the successful client login response.
         homeserver -- URL of homeserver, e.g. "https://matrix.example.org"
@@ -36,21 +36,25 @@ def write_details_to_disk(resp: LoginResponse, homeserver) -> None:
 
 
 async def main() -> None:
-
     # If there are no previously-saved credentials, we'll use the password
     if not os.path.exists(CONFIG_FILE):
         print("First time use. Did not find credential file. Asking for "
               "homeserver, user, and password to create credential file.")
         homeserver = "https://matrix.example.org"
         homeserver = input(f"Enter your homeserver URL: [{homeserver}] ")
+
         if not (homeserver.startswith("https://")
                 or homeserver.startswith("http://")):
             homeserver = "https://" + homeserver
+
         user_id = "@user:example.org"
         user_id = input(f"Enter your full user ID: [{user_id}] ")
+
         client = AsyncClient(homeserver, user_id)
         pw = getpass.getpass()
+
         resp = await client.login(pw)
+
         # check that we logged in succesfully
         if (isinstance(resp, LoginResponse)):
             write_details_to_disk(resp, homeserver)
@@ -70,6 +74,7 @@ async def main() -> None:
         with open(CONFIG_FILE, "r") as f:
             config = json.load(f)
             client = AsyncClient(config['homeserver'])
+
             client.access_token = config['access_token']
             client.user_id = config['user_id']
             client.device_id = config['device_id']
@@ -77,6 +82,7 @@ async def main() -> None:
         # Now we can send messages as the user
         room_id = "!myfavouriteroomid:example.org"
         room_id = input(f"Enter room id for test message: [{room_id}] ")
+
         await client.room_send(
             room_id,
             message_type="m.room.message",
@@ -91,4 +97,3 @@ async def main() -> None:
     await client.close()
 
 asyncio.get_event_loop().run_until_complete(main())
-
