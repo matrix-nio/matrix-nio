0a28eec26d9207c4c75c995a5d07c40d50057d65
Rémy Le Calloch <remy@lecalloch.net>
2023-03-06 22:44:57 +0100

    Fix: FormatChecker.cls_checks depreciated

--- nio/schemas.py
+++ nio/schemas.py
@@ -28,6 +28,7 @@ Base64Regex = r"[^-A-Za-z0-9+/=]|=[^=]|={3,}$"
 KeyRegex = r"(ed25519|curve25519):.+"
 SignedCurveRegex = r"(signed_curve25519|curve25519):.+"
 
+checker = FormatChecker()
 
 def extend_with_default(validator_class):
     validate_properties = validator_class.VALIDATORS["properties"]
@@ -46,7 +47,7 @@ def extend_with_default(validator_class):
 Validator = extend_with_default(Draft4Validator)
 
 
-@FormatChecker.cls_checks("user_id", ValueError)
+@checker.checks("user_id", ValueError)
 def check_user_id(value: str) -> bool:
     if not value.startswith("@"):
         raise ValueError("UserIDs start with @")
@@ -57,7 +58,7 @@ def check_user_id(value: str) -> bool:
     return True
 
 
-@FormatChecker.cls_checks("http_url", ValueError)
+@checker.checks("http_url", ValueError)
 def check_http_url(value: str) -> bool:
     if not re.match(r"^https?://.+", value):
         raise ValueError("Must be http://... or https://... URL")
