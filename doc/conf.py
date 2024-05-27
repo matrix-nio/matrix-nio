045f6fe5b4014ceea76016324a1de29ffda181de
Paarth Shah <mail@shahpaarth.com>
2024-05-27 15:44:56 -0700

    Publish 0.25.0rc1 + basic framework for trusted publishing (#501)

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ author = "Damir Jelić, Paarth Shah"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.24.0"
+release = "0.25.0rc1"
 
 
 # -- General configuration ---------------------------------------------------
